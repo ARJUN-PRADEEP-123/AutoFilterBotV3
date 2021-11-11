334d65342d5701cfaa7309a39456b49c38d5fee2
ARJUN-PRADEEP-123 <93504468+ARJUN-PRADEEP-123@users.noreply.github.com>
2021-11-11 16:14:56 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -18,12 +18,12 @@ AUTH_CHANNEL = None
 
 # MongoDB information
 DATABASE_URI = "mongodb://[LuciferMoringstar_Robot:LuciferMoringstar_Robot@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
-DATABASE_NAME = 'LuciferMoringstar_Robot'
+DATABASE_NAME = 'WRAYA'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
 # Messages
 START_MSG = """
-**Hi, I'm Media Search bot**
+**Hi, I'm Wraya**
 
 Here you can search files in inline mode. Just press follwing buttons and start searching.
 """
